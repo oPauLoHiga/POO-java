0e8ca0a0e5cffc784311dd2b6c03136445eca887
Paulo Higa <paulojhiga@gmail.com>
2025-09-02 09:45:56 -0300

    teste
    apagando linha.
    comentando "teste"

--- calculadora.java
+++ calculadora.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-
 public class calculadora {
     public static void main(String[] args) {
         double resultado = 0;
--- lanchonete.java
+++ lanchonete.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+// teste
 
 public class lanchonete {
     public static void main(String[] args) {
