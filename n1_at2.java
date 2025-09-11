700ae42b8a3fe59a7406c8f7526e28fc4c579bc7
Paulo Higa <paulojhiga@gmail.com>
2025-09-11 12:23:43 -0300

    n2 atualizada
    não terminada
    erro!!
    digitar 0

--- n1_at2.java
+++ n1_at2.java
@@ -3,8 +3,51 @@ import java.util.Scanner;
 public class n1_at2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        String[] produtos = {
+                "Cachaça 51", "Heineken",
+                "Guaraná", "Guaraná Zero",
+                "Batata Frita", "Torresmo",
+                "Feijoada", "Picanha"
+        };
+        double[] valores = {
+                5.00, 12.00,
+                7.00, 7.50,
+                20.00, 25.00,
+                40.00, 60.00
+        };
 
+        int[] pedidos = new int[2];
+        int[] quantidades = new int[2];
+
+        System.out.print("Digite o número da mesa: ");
+        int mesa = sc.nextInt();
+
+        System.out.println("\n****** CARDAPIO ******");
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.printf("%d - %s (R$:%.2f) \n", i+1, produtos[i], valores[i]);
+        }
+
+        for (int i = 0; i < 2; i++) {
+
+            System.out.printf("\nDigite o numero do produto para o pedido %d: ", i + 1);
+            pedidos[i] = sc.nextInt() - 1;
+            if (pedidos[i] < 1 || pedidos[i] > produtos.length) {
+                System.out.println("PRODUTO INVALIDO!!!");
+                continue;
+            }
+            System.out.printf("Digite a quantidade: ");
+            quantidades[i] = sc.nextInt();
+        }
+
+                System.out.println("********** CONTA DA MESA" + mesa +"**********");
+                double total = 0;
+                for (int i = 0; i < 2; i++){
+                    int indice = pedidos[i];
+                    double subtotal = valores[indice] * quantidades[i];
+                    total += subtotal;
+                    System.out.printf("\n%d x %s (R$%.2f cada) = R$%.2f",quantidades[i], produtos[i],valores[indice], subtotal);
+                }
+                 System.out.printf("\nTOTAL A PAGAR: R$ %.2f\n", total);
 
         sc.close();
     }
