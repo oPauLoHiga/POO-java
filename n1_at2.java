846632ea2e7e8e0f39270f99a8c2f984e6d72f70
Paulo Higa <paulojhiga@gmail.com>
2025-09-17 20:48:57 -0300

    Update n1_at2.java

--- n1_at2.java
+++ n1_at2.java
@@ -24,30 +24,44 @@ public class n1_at2 {
 
         System.out.println("\n****** CARDAPIO ******");
         for (int i = 0; i < produtos.length; i++) {
-            System.out.printf("%d - %s (R$:%.2f) \n", i+1, produtos[i], valores[i]);
+            System.out.printf("%d - %s (R$:%.2f) \n", i + 1, produtos[i], valores[i]);
         }
 
         for (int i = 0; i < 2; i++) {
 
             System.out.printf("\nDigite o numero do produto para o pedido %d: ", i + 1);
-            pedidos[i] = sc.nextInt() - 1;
-            if (pedidos[i] < 1 || pedidos[i] > produtos.length) {
+            pedidos[i] = sc.nextInt();
+
+            while (pedidos[i] <= 0 || pedidos[i] > produtos.length) {
+                System.out.println("\nOi1");
                 System.out.println("PRODUTO INVALIDO!!!");
-                continue;
+
+
+                System.out.printf("\nDigite outro numero do produto para o pedido %d: ", i + 1);
+                pedidos[i] = sc.nextInt();
             }
+
             System.out.printf("Digite a quantidade: ");
             quantidades[i] = sc.nextInt();
+
+            while (quantidades[i] < 1) {
+
+                System.out.println("QUANTIDADE INVALIDA!!!");
+
+                System.out.printf("Digite a quantidade: ");
+                quantidades[i] = sc.nextInt();
+            }
         }
 
-                System.out.println("********** CONTA DA MESA" + mesa +"**********");
-                double total = 0;
-                for (int i = 0; i < 2; i++){
-                    int indice = pedidos[i];
-                    double subtotal = valores[indice] * quantidades[i];
-                    total += subtotal;
-                    System.out.printf("\n%d x %s (R$%.2f cada) = R$%.2f",quantidades[i], produtos[i],valores[indice], subtotal);
-                }
-                 System.out.printf("\nTOTAL A PAGAR: R$ %.2f\n", total);
+        System.out.println("********** CONTA DA MESA" + mesa + "**********");
+        double total = 0;
+        for (int i = 0; i < 2; i++) {
+            int indice = pedidos[i];
+            double subtotal = valores[indice] * quantidades[i];
+            total += subtotal;
+            System.out.printf("\n%d x %s (R$%.2f cada) = R$%.2f", quantidades[i], produtos[i], valores[indice], subtotal);
+        }
+        System.out.printf("\nTOTAL A PAGAR: R$ %.2f\n", total);
 
         sc.close();
     }
