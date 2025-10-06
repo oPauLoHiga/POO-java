700faab3ba0a4e856a3018099b2f16453e7aeed6
PAULO JOSÉ HIGA FREITAS <paulo.higa@a.ucb.br>
2025-10-06 20:43:04 -0300

    herança
    aula 06/10/25
    basico de herança

---
+++ br/com/app/Progama.java
@@ -0,0 +1,28 @@
+package br.com.app;
+import br.com.entities.Livro;
+
+import java.util.Scanner;
+
+public class Progama {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //cadastro
+
+        System.out.println("Titulo do Livro");
+        String titulo = sc.nextLine();
+
+        System.out.println("Autor:");
+        String autor = sc.nextLine();
+
+        System.out.println("Preço: ");
+        double preco = sc.nextDouble();
+
+        Livro livro = new Livro(titulo, autor, preco);
+
+        //impressão
+        System.out.println(livro);
+
+        sc.close();
+    }
+}
---
+++ br/com/entities/Livro.java
@@ -0,0 +1,36 @@
+package br.com.entities;
+
+public class Livro {
+    public String titulo;
+    public String autor;
+    public double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String toString(){
+        return "Livro = Titulo:"+ titulo + " Autor: "+ autor + " Preço: " + preco;
+    }
+}
