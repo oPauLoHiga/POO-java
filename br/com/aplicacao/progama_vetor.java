f5de9fb0db43a70b5443dbc688e043b84c7cd28a
Paulo Higa <paulojhiga@gmail.com>
2025-10-20 20:30:08 -0300

    Update progama_vetor.java

--- br/com/aplicacao/progama_vetor.java
+++ br/com/aplicacao/progama_vetor.java
@@ -5,6 +5,7 @@ public class progama_vetor {
         Scanner sc = new Scanner(System.in);
         double [] valor = new double[4];
         String [] produto = new String [4];
+        
         //CADASTRO
         for (int i = 0; i < produto.length; i++) {
             System.out.printf("Digite o %d nome do produto: ", i + 1);
@@ -13,10 +14,18 @@ public class progama_vetor {
             valor[i] = sc.nextDouble();
             sc.nextLine();
         }
+
+        //SOMA
+        double total = 0;
+        for (int i = 0; i < produto.length; i++) {
+            total += valor[i];
+        }
+
         //IMPRESSÃO
         for (int i = 0; i < 4; i++){
-            System.out.printf("Produto: %s | Valor: %.2f%n%n", produto[i], valor[i]);
+            System.out.printf("Produto: %s | Valor: %.2f%n", produto[i], valor[i]);
         }
+        System.out.printf("Total: R$ %.2f%n%n", total);
         sc.close();
     }
 }
